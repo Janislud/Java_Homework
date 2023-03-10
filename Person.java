7ef8c713633f2ac6f43a9640bb075fc2db52a44f
Janislud <90716004+Janislud@users.noreply.github.com>
2023-03-10 16:52:15 +0300

    Homework

---
+++ Person.java
@@ -0,0 +1,10 @@
+package org.person;
+
+public class Person {
+
+    public String name = "Janis";
+    public String surname = "Ludborzs";
+    public int age = 26;
+    public static int yearOfBirth= 1997;
+
+}
